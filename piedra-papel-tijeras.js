2364f996667959255a0446069124bd9bb6f3f0b7
hei-ola <lorentsvan@outlook.com>
2021-09-15 17:25:01 -0500

    cambios y mejoras

--- piedra-papel-tijeras.js
+++ piedra-papel-tijeras.js
@@ -1,123 +1,52 @@
-// let edad = parseInt(prompt("Ingrese su edad"));
+let empezarPartida =parseInt(prompt("JUGUEMOS!! piedra digita 1, papel digita 2, tijeras digita 3"));
 
-// let bandera = 0;
+Partida(empezarPartida);
 
-// while (bandera == 0) {   
-//     if (isNaN(edad)) {       
-//         edad = prompt("Ingreso un dato incorrecto... ingrese su edad");
-//     } else {
-//         if (edad > 18) {
-//             console.log("Es mayor de edad");
-//         } else {
-//             console.log("Es menor de edad");
-//         };
-//         bandera = 1;        
-//     };
-// };
+function Partida(ingresoUsuario) {
 
-
-//JUEGO DE PIEDRA, PAPEL Y TIJERA
-let jueguemos = confirm("¿Quiere jugar piedra-papel-tijeras?");
-let cont = 0;
-
-if (jueguemos) {
-    while (cont == 0) {
-        let juego = parseInt(prompt("JUGUEMOS!! piedra digita 1, papel digita 2, tijeras digita 3 no da el cancelar jajajja se jodio pa que dijo q si queria jugar"));
+    if (!isNaN(ingresoUsuario)) {
+        console.log("ingreso " + ingresoUsuario);
         let maquina = parseInt(Math.random() * 3 + 1);
-        if (isNaN(juego) || juego > 3) {
-            juego = prompt("Ingreso un dato incorrecto... piedra digita 1, papel digita 2, tijeras digita 3 no da el cancelar jajajja se jodio pa que dijo q si queria jugar");
-        } else {            
-            if (juego == 1) {
-                if (maquina == 1) {
-                    alert("empatee piedra - piedra");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                } else if (maquina == 2) {
-                    alert("perdiste!! :( .... papel - piedra");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                } else {
-                    alert("ganaste :) !!.... tijeras - piedra");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                };
 
-            } else if (juego == 2) {
-                if (maquina == 1) {
-                    alert("ganaste!! :) .... piedra - papel");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                } else if (maquina == 2) {
-                    alert("empate!!.... papel - papel");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                } else {
-                    alert("perdiste!! :( .... tijeras - papel");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                };
-
-            } else {
-                if (maquina == 1) {
-                    alert("perdiste!!! :( .... piedra - tijeras");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                } else if (maquina == 2) {
-                    alert("ganaste!! :)  papel - tijeras");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                } else {
-                    alert("empate .... tijeras - tijeras");
-                    let otraVez = confirm("¿Desea seguir jugando?");
-                    if (otraVez) {
-                        cont = 0;
-                        maquina = parseInt(Math.random() * 3 + 1);
-                    } else {
-                        cont = 1;
-                    };
-                };
+        if (maquina === ingresoUsuario) {
+            alert("Empataste.....😎");
+        };
+        //si ingresa piedra
+        if (ingresoUsuario == 1) {
+            if (maquina == 2) {
+                alert("GANASTE!!!.....🤩");
+            } else if(maquina == 3) {
+                alert("PERDISTE!!!.....😪");
             };
 
-        };
+            //si ingresa papel
+        } else if (ingresoUsuario == 2) {
+            if (maquina == 1) {
+                alert("GANASTE!!!.....🤩");
+            } else if(maquina == 3) {
+                alert("PERDISTE!!!.....😪");
+            };
+        } else {
+            //si ingresa tijeras
+            if (maquina == 1) {
+                alert("PERDISTE!!!.....😪");
+            } else if(maquina == 2){
+                alert("GANASTE!!!.....🤩");
+            };
+        }; 
+        otroIntento();       
+    };    
+};
+
+function otroIntento() {
+    let otraVez = confirm("¿Desea seguir jugando?");
+    if (otraVez) {
+        empezarPartida = parseInt(prompt("piedra digita 1, papel digita 2, tijeras digita 3"));
+        Partida(empezarPartida);
     };
 };
+
+
+
+
+
